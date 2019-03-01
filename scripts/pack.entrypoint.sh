6f5db555997f3625f441bcdffec9f29c8827893a
mikhail.baleika <mikhail.baleika@gmail.com>
2019-03-01 17:58:26 +0300

    CI fixup

--- scripts/pack.entrypoint.sh
+++ scripts/pack.entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd ..
 cd /src/Cortlex.Rescope 
-dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope /p:Version=
+dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope /p:Version=$1
 cd ..
 
 cd /src/Cortlex.Rescope.Autofac
-dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope.Autofac /p:Version=
+dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope.Autofac /p:Version=$1
 cd ..
 
 cd /src/Cortlex.Rescope.CastleWindsor
-dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope.CastleWindsor /p:Version=
+dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope.CastleWindsor /p:Version=$1
 cd ..
 
 cd /src/Cortlex.Rescope.NETCore
-dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope.NETCore /p:Version=
+dotnet pack -c Release --no-build -o /var/temp/pack/Cortlex.Rescope.NETCore /p:Version=$1
 cd ..
